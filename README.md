c0b6a3ea87bb871733f943d3d69efb0248628e47
honkyrot <111529160+honkyrot@users.noreply.github.com>
2024-03-29 01:05:43 -0400

    rework
    REMAKING THE WEBSITE, BUILD 1

--- README.md
+++
@@ -1,7 +0,0 @@
-# honkyrot.github.io
-A small personal website about me and my portfolio; that's about it.
-Expect my code to be mid? I don't know but it's alright for me.
-I'm still trying to learn, so if you want to help, great!
-You are free to explore my website and stuff.
-
-This repository is now public and available for viewing.
--- ajax_loader.js
+++ ajax_loader.js
@@ -1,25 +1,37 @@
 // This file contains the functions that are used to load the pages
-// Also a small transition to make the page look better
+// Also a small transition to make the page look better\
+
+const ajax_body = document.getElementById("ajax_body");
+const ajax_title = document.getElementById("page_title_text");  // title_title is the title of the page (e.g. Home, About, etc.)
+const main_title = document.getElementById("main_title");  // div containing the title
+const title_decor = document.getElementById("title_decor");  // bg of title
+let current_page = null;  // current page is home by default
 
 function load_page(page_name) {  // page_name is a string
+    if (page_name == current_page) {
+        // skip loading the page if it's already loaded
+        return;
+    }
+
+    current_page = page_name;
     var xhttp = new XMLHttpRequest();
-    const ajax_body = document.getElementById("ajax_body");
-    const title_title = document.getElementById("title_title");  // title_title is the title of the page (e.g. Home, About, etc.)
     const capitalized = page_name.charAt(0).toUpperCase() + page_name.slice(1);  // Capitalize the first letter of the page name
-    var page_title = "<h1>" + capitalized + "</h1>";
+    var page_title = capitalized
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var temp_storage = this.responseText
         document.querySelector(".ajax_body_span").style.opacity = "0";
-        document.querySelector(".title_title").style.opacity = "0";
+        document.querySelector(".page_title_text").style.opacity = "0";
+
         setTimeout(function() {  // I AAAa TRANSITIONS
             document.querySelector(".ajax_body_span").style.opacity = "1";
-            document.querySelector(".title_title").style.opacity = "1";
+            document.querySelector(".page_title_text").style.opacity = "1";
             //document.querySelector(".white_line_1_name_title_header").style.width = (page_name.length * 12) + 520 + "px";
             ajax_body.innerHTML = temp_storage;
-            title_title.innerHTML = page_title;
-            update_audio_player_div_position();
-        }, 300);  // 300ms
+            ajax_title.innerHTML = page_title;
+
+            title_decor.style.width = main_title.offsetWidth + 40 + "px";
+        }, 200);  // 200ms
         }
     };
     var file_name = "pages/" + page_name + ".html";
@@ -27,12 +39,15 @@ function load_page(page_name) {  // page_name is a string
     xhttp.send();  // does whatever ajax does
 }
 const debug = false;
+var website_loaded = false;
+
+const main_title_text = document.getElementById("main_title_text");
 
 // load home page on page load
 window.onload = function() {
     if (!debug) {
         load_page("home");
-        update_audio_player();
+        //load_page("home");
     } else {
         console.log("Debug mode is on, not loading page");
     }
